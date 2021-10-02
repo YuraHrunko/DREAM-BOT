26382bf4053aebaa8d2ed2a7e8ec5141aed4aeeb
YuraHrunko <47062666+YuraHrunko@users.noreply.github.com>
2021-10-02 21:52:13 +0300

    1

--- actions/check_member_data.js
+++ actions/check_member_data.js
@@ -1,172 +1,191 @@
 module.exports = {
-	//---------------------------------------------------------------------
-	// Action Name
-	//
-	// This is the name of the action displayed in the editor.
-	//---------------------------------------------------------------------
+  name: 'Check Member Data',
+  section: 'Data',
 
-	name: "Check Member Data",
+  subtitle(data) {
+    const results = [
+      'Continue Actions',
+      'Stop Action Sequence',
+      'Jump To Action',
+      'Jump Forward Actions',
+      'Jump to Anchor',
+    ];
+    return `If True: ${results[parseInt(data.iftrue, 10)]} ~ If False: ${results[parseInt(data.iffalse, 10)]}`;
+  },
 
-	//---------------------------------------------------------------------
-	// Action Section
-	//
-	// This is the section the action will fall into.
-	//---------------------------------------------------------------------
+  fields: ['member', 'varName', 'dataName', 'comparison', 'value', 'iftrue', 'iftrueVal', 'iffalse', 'iffalseVal'],
 
-	section: "Data",
-
-	//---------------------------------------------------------------------
-	// Action Subtitle
-	//
-	// This function generates the subtitle displayed next to the name.
-	//---------------------------------------------------------------------
-
-	subtitle: function(data) {
-		const results = ["Continue Actions", "Stop Action Sequence", "Jump To Action", "Jump Forward Actions"];
-		return `If True: ${results[parseInt(data.iftrue)]} ~ If False: ${results[parseInt(data.iffalse)]}`;
-	},
-
-	//---------------------------------------------------------------------
-	// Action Fields
-	//
-	// These are the fields for the action. These fields are customized
-	// by creating elements with corresponding IDs in the HTML. These
-	// are also the names of the fields stored in the action's JSON data.
-	//---------------------------------------------------------------------
-
-	fields: ["member", "varName", "dataName", "comparison", "value", "iftrue", "iftrueVal", "iffalse", "iffalseVal"],
-
-	//---------------------------------------------------------------------
-	// Command HTML
-	//
-	// This function returns a string containing the HTML used for
-	// editting actions.
-	//
-	// The "isEvent" parameter will be true if this action is being used
-	// for an event. Due to their nature, events lack certain information,
-	// so edit the HTML to reflect this.
-	//
-	// The "data" parameter stores constants for select elements to use.
-	// Each is an array: index 0 for commands, index 1 for events.
-	// The names are: sendTargets, members, roles, channels,
-	//                messages, servers, variables
-	//---------------------------------------------------------------------
-
-	html: function(isEvent, data) {
-		return `
+  html(isEvent, data) {
+    return `
+<div><p>This action has been modified by DBM Mods.</p></div><br>
 <div>
-	<div style="float: left; width: 35%;">
-		Member:<br>
-		<select id="member" class="round" onchange="glob.memberChange(this, 'varNameContainer')">
-			${data.members[isEvent ? 1 : 0]}
-		</select>
-	</div>
-	<div id="varNameContainer" style="display: none; float: right; width: 60%;">
-		Variable Name:<br>
-		<input id="varName" class="round" type="text" list="variableList">
-	</div>
+  <div style="float: left; width: 35%;">
+    Member:<br>
+    <select id="member" class="round" onchange="glob.memberChange(this, 'varNameContainer')">
+      ${data.members[isEvent ? 1 : 0]}
+    </select>
+  </div>
+  <div id="varNameContainer" style="display: none; float: right; width: 60%;">
+    Variable Name:<br>
+    <input id="varName" class="round" type="text" list="variableList">
+  </div>
 </div><br><br><br>
 <div style="padding-top: 8px;">
-	<div style="float: left; width: 50%;">
-		Data Name:<br>
-		<input id="dataName" class="round" type="text">
-	</div>
-	<div style="float: left; width: 45%;">
-		Comparison Type:<br>
-		<select id="comparison" class="round">
-			<option value="0">Exists</option>
-			<option value="1" selected>Equals</option>
-			<option value="2">Equals Exactly</option>
-			<option value="3">Less Than</option>
-			<option value="4">Greater Than</option>
-			<option value="5">Includes</option>
-			<option value="6">Matches Regex</option>
-		</select>
-	</div>
+  <div style="float: left; width: 50%;">
+    Data Name:<br>
+    <input id="dataName" class="round" type="text">
+  </div>
+  <div style="float: left; width: 45%;">
+    Comparison Type:<br>
+    <select id="comparison" class="round">
+      <option value="0">Exists</option>
+      <option value="1" selected>Equals</option>
+      <option value="2">Equals Exactly</option>
+      <option value="3">Less Than</option>
+      <option value="4">Greater Than</option>
+      <option value="5">Includes</option>
+      <option value="6">Matches Regex</option>
+      <option value="7">Length is Bigger Than</option>
+      <option value="8">Length is Smaller Than</option>
+      <option value="9">Length Equals</option>
+      <option value="10">Starts With</option>
+      <option value="11">Ends With</option>
+    </select>
+  </div>
 </div><br><br><br>
 <div style="padding-top: 8px;">
-	Value to Compare to:<br>
-	<input id="value" class="round" type="text" name="is-eval">
+  Value to Compare to:<br>
+  <input id="value" class="round" type="text" name="is-eval">
 </div>
 <div style="padding-top: 16px;">
-	${data.conditions[0]}
+  ${data.conditions[0]}
 </div>`;
-	},
+  },
 
-	//---------------------------------------------------------------------
-	// Action Editor Init Code
-	//
-	// When the HTML is first applied to the action editor, this code
-	// is also run. This helps add modifications or setup reactionary
-	// functions for the DOM elements.
-	//---------------------------------------------------------------------
+  init() {
+    const { glob, document } = this;
+    const option = document.createElement('OPTION');
+    option.value = '4';
+    option.text = 'Jump to Anchor';
+    const iffalse = document.getElementById('iffalse');
+    if (iffalse.length === 4) iffalse.add(option);
 
-	init: function() {
-		const { glob, document } = this;
+    const option2 = document.createElement('OPTION');
+    option2.value = '4';
+    option2.text = 'Jump to Anchor';
+    const iftrue = document.getElementById('iftrue');
+    if (iftrue.length === 4) iftrue.add(option2);
 
-		glob.memberChange(document.getElementById("member"), "varNameContainer");
-		glob.onChangeTrue(document.getElementById("iftrue"));
-		glob.onChangeFalse(document.getElementById("iffalse"));
-	},
+    glob.onChangeTrue = function onChangeTrue(event) {
+      switch (parseInt(event.value, 10)) {
+        case 0:
+        case 1:
+          document.getElementById('iftrueContainer').style.display = 'none';
+          break;
+        case 2:
+          document.getElementById('iftrueName').innerHTML = 'Action Number';
+          document.getElementById('iftrueContainer').style.display = null;
+          break;
+        case 3:
+          document.getElementById('iftrueName').innerHTML = 'Number of Actions to Skip';
+          document.getElementById('iftrueContainer').style.display = null;
+          break;
+        case 4:
+          document.getElementById('iftrueName').innerHTML = 'Anchor ID';
+          document.getElementById('iftrueContainer').style.display = null;
+          break;
+        default:
+          break;
+      }
+    };
+    glob.onChangeFalse = function onChangeFalse(event) {
+      switch (parseInt(event.value, 10)) {
+        case 0:
+        case 1:
+          document.getElementById('iffalseContainer').style.display = 'none';
+          break;
+        case 2:
+          document.getElementById('iffalseName').innerHTML = 'Action Number';
+          document.getElementById('iffalseContainer').style.display = null;
+          break;
+        case 3:
+          document.getElementById('iffalseName').innerHTML = 'Number of Actions to Skip';
+          document.getElementById('iffalseContainer').style.display = null;
+          break;
+        case 4:
+          document.getElementById('iffalseName').innerHTML = 'Anchor ID';
+          document.getElementById('iffalseContainer').style.display = null;
+          break;
+        default:
+          break;
+      }
+    };
+    glob.memberChange(document.getElementById('member'), 'varNameContainer');
+    glob.onChangeTrue(document.getElementById('iftrue'));
+    glob.onChangeFalse(document.getElementById('iffalse'));
+  },
 
-	//---------------------------------------------------------------------
-	// Action Bot Function
-	//
-	// This is the function for the action within the Bot's Action class.
-	// Keep in mind event calls won't have access to the "msg" parameter,
-	// so be sure to provide checks for variable existance.
-	//---------------------------------------------------------------------
+  action(cache) {
+    const data = cache.actions[cache.index];
+    const type = parseInt(data.member, 10);
+    const varName = this.evalMessage(data.varName, cache);
+    const member = this.getMember(type, varName, cache);
+    let result = false;
 
-	action: function(cache) {
-		const data = cache.actions[cache.index];
-		const type = parseInt(data.member);
-		const varName = this.evalMessage(data.varName, cache);
-		const member = this.getMember(type, varName, cache);
-		let result = false;
-		if(member && member.data) {
-			const dataName = this.evalMessage(data.dataName, cache);
-			const val1 = member.data(dataName);
-			const compare = parseInt(data.comparison);
-			let val2 = this.evalMessage(data.value, cache);
-			if(compare !== 6) val2 = this.eval(val2, cache);
-			if(val2 === false) val2 = this.evalMessage(data.value, cache);
-			switch(compare) {
-				case 0:
-					result = val1 !== undefined;
-					break;
-				case 1:
-					result = val1 == val2;
-					break;
-				case 2:
-					result = val1 === val2;
-					break;
-				case 3:
-					result = val1 < val2;
-					break;
-				case 4:
-					result = val1 > val2;
-					break;
-				case 5:
-					if(typeof(val1.includes) === "function") {
-						result = val1.includes(val2);
-					}
-					break;
-				case 6:
-					result = Boolean(val1.match(new RegExp(`^${val2}$`, "i")));
-					break;
-			}
-		}
-		this.executeResults(result, data, cache);
-	},
+    if (member && member.data) {
+      const dataName = this.evalMessage(data.dataName, cache);
+      const val1 = member.data(dataName);
+      const compare = parseInt(data.comparison, 10);
+      let val2 = this.evalMessage(data.value, cache);
+      if (compare !== 6) val2 = this.eval(val2, cache);
+      if (val2 === false) val2 = this.evalMessage(data.value, cache);
 
-	//---------------------------------------------------------------------
-	// Action Bot Mod
-	//
-	// Upon initialization of the bot, this code is run. Using the bot's
-	// DBM namespace, one can add/modify existing functions if necessary.
-	// In order to reduce conflictions between mods, be sure to alias
-	// functions you wish to overwrite.
-	//---------------------------------------------------------------------
+      switch (compare) {
+        case 0:
+          result = val1 !== undefined;
+          break;
+        case 1:
+          // eslint-disable-next-line eqeqeq
+          result = val1 == val2;
+          break;
+        case 2:
+          result = val1 === val2;
+          break;
+        case 3:
+          result = val1 < val2;
+          break;
+        case 4:
+          result = val1 > val2;
+          break;
+        case 5:
+          if (typeof val1.includes === 'function') {
+            result = val1.includes(val2);
+          }
+          break;
+        case 6:
+          result = Boolean(val1.match(new RegExp(`^${val2}$`, 'i')));
+          break;
+        case 7:
+          result = val1.length > val2;
+          break;
+        case 8:
+          result = val1.length < val2;
+          break;
+        case 9:
+          result = val1.length === val2;
+          break;
+        case 10:
+          result = val1.startsWith(val2);
+          break;
+        case 11:
+          result = val1.endsWith(val2);
+          break;
+        default:
+          break;
+      }
+    }
+    this.executeResults(result, data, cache);
+  },
 
-	mod: function() {}
-}; // End of module
+  mod() {},
+};
--- actions/run_script.js
+++ actions/run_script.js
@@ -1,151 +1,308 @@
 module.exports = {
-	//---------------------------------------------------------------------
-	// Action Name
-	//
-	// This is the name of the action displayed in the editor.
-	//---------------------------------------------------------------------
+  name: 'Run Script',
+  section: 'Other Stuff',
 
-	name: "Run Script",
+  subtitle(data) {
+    return `${data.code}`;
+  },
 
-	//---------------------------------------------------------------------
-	// Action Section
-	//
-	// This is the section the action will fall into.
-	//---------------------------------------------------------------------
+  variableStorage(data, varType) {
+    if (parseInt(data.storage, 10) !== varType) return;
+    let dataType = 'Unknown Type';
+    switch (parseInt(data.VTypeSelect, 10)) {
+      case 0:
+        dataType = 'Unknown Type';
+        break;
+      case 1:
+        dataType = 'Number';
+        break;
+      case 2:
+        dataType = 'String';
+        break;
+      case 3:
+        dataType = 'Image';
+        break;
+      case 4:
+        dataType = 'Member';
+        break;
+      case 5:
+        dataType = 'Message';
+        break;
+      case 6:
+        dataType = 'Text Channel';
+        break;
+      case 7:
+        dataType = 'Voice Channel';
+        break;
+      case 8:
+        dataType = 'Role';
+        break;
+      case 9:
+        dataType = 'Server';
+        break;
+      case 10:
+        dataType = 'Emoji';
+        break;
+      case 11:
+        dataType = data.CVTypeValue;
+        break;
+    }
+    return [data.varName, dataType];
+  },
 
-	section: "Other Stuff",
+  fields: ['code', 'behavior', 'interpretation', 'storage', 'varName', 'VTypeSelect', 'CVTypeValue'],
 
-	//---------------------------------------------------------------------
-	// Action Subtitle
-	//
-	// This function generates the subtitle displayed next to the name.
-	//---------------------------------------------------------------------
+  html(_isEvent, data) {
+    return `
+<div id ="wrexdiv" style="width: 570px; height: 359px; overflow-x: hidden;">
+  <div style="width: 100%; height: 324px;">
+    <iframe id="JSEditor" style="visibility: hidden; width: 100%; height: 320px;" frameBorder="0"></iframe>
+    <textarea id="code" name="is-eval" style="display: none; width: 100%; height: 320px; white-space: nowrap; resize: none;"></textarea>
+  </div>
+  <div style="width: 100%; height: 39px; background-color: #161616; margin-top: -4px; user-select: none;">
+    <button class="RS_button" onclick="glob.onClickDefault(this)">Default</button>
+    <span style="font-family:Verdana, Geneva, sans-serif; opacity: .3; font-size: 20px; color: white; position: relative; top: 2px; left: 3px;">Run Script V2</span><br>
+    <span style="font-family:Verdana, Geneva, sans-serif; opacity: .2; font-size: 11.9px; color: white; position: relative; top: -1px; left: 4px;">Modification by MrGold</span>
+  </div>
+  <div style="padding-left: 17px; padding-top: 17px;">
+    <div>
+      <div style="float: left; width: 257px;">
+        End Behavior:<br>
+        <select id="behavior" class="round">
+          <option value="0" selected>Call Next Action Automatically</option>
+          <option value="1">Do Not Call Next Action</option>
+        </select>
+      </div>
+      <div style="padding-left: 5%; float: left; width: 255px;">
+        Interpretation Style:<br>
+        <select id="interpretation" class="round">
+          <option value="0">Evaluate Code First</option>
+          <option value="1" selected>Evaluate Code Directly</option>
+        </select>
+      </div>
+    </div><br><br><br>
+    <div>
+      <div style="float: left; width: 35%;">
+        Store In:<br>
+        <select id="storage" class="round" onchange="glob.variableChange(this, 'varNameContainer'); glob.onChangeVTypeSelect1()">
+          ${data.variables[0]}
+        </select>
+      </div>
+      <div id="varNameContainer" style="display: none; float: right; width: 60%;">
+        Variable Name:<br>
+        <input id="varName" class="round" type="text">
+      </div>
+    </div>
+    <div id="VTypeDiv" style="display: none;"><br><br><br>
+      <div style="float: left; width: 35%;">
+        Variable Type:<br>
+        <select id="VTypeSelect" class="round" onchange="glob.onChangeVTypeSelect2(this)">
+          <option value="0" selected>Unknown Type</option>
+          <option value="1">Number</option>
+          <option value="2">String</option>
+          <option value="3">Image</option>
+          <option value="4">Member</option>
+          <option value="5">Message</option>
+          <option value="6">Text Channel</option>
+          <option value="7">Voice Channel</option>
+          <option value="8">Role</option>
+          <option value="9">Server</option>
+          <option value="10">Emoji</option>
+          <option value="11">Custom Variable Type</option>
+        </select>
+      </div>
+      <div id="CVTypeDiv" style="display: none; float: right; width: 60%;">
+        Custom Variable Type Name:<br>
+        <input id="CVTypeValue" class="round" type="text">
+      </div>
+    </div><br><br><br><br>
+  </div>
+</div>
+<style>
+  .action-input {
+    margin: 0 !important;
+    padding: 0px !important;
+  }
 
-	subtitle: function(data) {
-		return `${data.code}`;
-	},
+  .RS_button {
+    float: right;
+    border: none;
+    outline: 0;
+    height: 100%;
+    width: 100px;
+    font-size: 25px;
+    font-family: Arial;
+    background-color: #2d2d2d;
+    color: white;
+    transition: background-color .1s;
+    cursor: pointer;
+  }
 
-	//---------------------------------------------------------------------
-	// Action Storage Function
-	//
-	// Stores the relevant variable info for the editor.
-	//---------------------------------------------------------------------
+  .RS_button:hover {
+    background-color: #3b3c3d;
+  }
 
-	variableStorage: function(data, varType) {
-		const type = parseInt(data.storage);
-		if(type !== varType) return;
-		return ([data.varName, "Unknown Type"]);
-	},
+  .RS_button:active {
+    background-color: #404142;
+  }
+</style>`;
+  },
 
-	//---------------------------------------------------------------------
-	// Action Fields
-	//
-	// These are the fields for the action. These fields are customized
-	// by creating elements with corresponding IDs in the HTML. These
-	// are also the names of the fields stored in the action's JSON data.
-	//---------------------------------------------------------------------
+  init() {
+    const { glob, document } = this;
 
-	fields: ["behavior", "interpretation", "code", "storage", "varName"],
+    document.getElementById('JSEditor').src =
+      'data:text/html,' +
+      `
+<script src="https://cdnjs.cloudflare.com/ajax/libs/ace/1.4.3/ace.js" integrity="sha256-gkWBmkjy/8e1QUz5tv4CCYgEtjR8sRlGiXsMeebVeUo=" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/ace/1.4.3/ext-language_tools.js" integrity="sha256-PAtX04Rk2WBELn+z4CmyvM2E5bhFBNEplF8mrHBvMJc=" crossorigin="anonymous"></script>
 
-	//---------------------------------------------------------------------
-	// Command HTML
-	//
-	// This function returns a string containing the HTML used for
-	// editting actions.
-	//
-	// The "isEvent" parameter will be true if this action is being used
-	// for an event. Due to their nature, events lack certain information,
-	// so edit the HTML to reflect this.
-	//
-	// The "data" parameter stores constants for select elements to use.
-	// Each is an array: index 0 for commands, index 1 for events.
-	// The names are: sendTargets, members, roles, channels,
-	//                messages, servers, variables
-	//---------------------------------------------------------------------
+<div id="editor"></div>
+<textarea id="hideCode" style="display: none;"></textarea>
 
-	html: function(isEvent, data) {
-		return `
-<div>
-	<div style="float: left; width: 45%;">
-		End Behavior:<br>
-		<select id="behavior" class="round">
-			<option value="0" selected>Call Next Action Automatically</option>
-			<option value="1">Do Not Call Next Action</option>
-		</select>
-	</div>
-	<div style="padding-left: 5%; float: left; width: 55%;">
-		Interpretation Style:<br>
-		<select id="interpretation" class="round">
-			<option value="0" selected>Evaluate Text First</option>
-			<option value="1">Evaluate Text Directly</option>
-		</select>
-	</div>
-</div><br><br><br>
-<div style="padding-top: 8px;">
-	Custom Code:<br>
-	<textarea id="code" rows="9" name="is-eval" style="width: 99%; white-space: nowrap; resize: none;"></textarea>
-</div><br>
-<div>
-	<div style="float: left; width: 35%;">
-		Store In:<br>
-		<select id="storage" class="round" onchange="glob.variableChange(this, 'varNameContainer')">
-			${data.variables[0]}
-		</select>
-	</div>
-	<div id="varNameContainer" style="display: none; float: right; width: 60%;">
-		Variable Name:<br>
-		<input id="varName" class="round" type="text">
-	</div>
-</div>`;
-	},
+<script>
+  var editor = ace.edit("editor", {
+    mode: "ace/mode/javascript",
+    theme: "ace/theme/monokai"
+  });
 
-	//---------------------------------------------------------------------
-	// Action Editor Init Code
-	//
-	// When the HTML is first applied to the action editor, this code
-	// is also run. This helps add modifications or setup reactionary
-	// functions for the DOM elements.
-	//---------------------------------------------------------------------
+  editor.setOptions({
+    enableBasicAutocompletion: true,
+    enableLiveAutocompletion: true,
+    autoScrollEditorIntoView: true,
+    showPrintMargin: false
+  });
 
-	init: function() {
-		const { glob, document } = this;
+  document.getElementById("hideCode").addEventListener("_load", function() {
+    editor.session.setValue(document.getElementById("hideCode").value);
+  });
 
-		glob.variableChange(document.getElementById("storage"), "varNameContainer");
-	},
+  editor.session.on('change', function() {
+    document.getElementById("hideCode").value = editor.getValue();
+  });
+</script>
+<style>
+  #editor {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    font-size: 12.1px;
+  }
 
-	//---------------------------------------------------------------------
-	// Action Bot Function
-	//
-	// This is the function for the action within the Bot's Action class.
-	// Keep in mind event calls won't have access to the "msg" parameter,
-	// so be sure to provide checks for variable existance.
-	//---------------------------------------------------------------------
+  ::-webkit-scrollbar {
+    width: 13px;
+  }
 
-	action: function(cache) {
-		const data = cache.actions[cache.index];
-		let code;
-		if(data.interpretation === "0") {
-			code = this.evalMessage(data.code, cache);
-		} else {
-			code = data.code;
-		}
-		const result = this.eval(code, cache);
-		const varName = this.evalMessage(data.varName, cache);
-		const storage = parseInt(data.storage);
-		this.storeValue(result, storage, varName, cache);
-		if(data.behavior === "0") {
-			this.callNextAction(cache);
-		}
-	},
+  ::-webkit-scrollbar-track {
+    background: #38393a;
+  }
 
-	//---------------------------------------------------------------------
-	// Action Bot Mod
-	//
-	// Upon initialization of the bot, this code is run. Using the bot's
-	// DBM namespace, one can add/modify existing functions if necessary.
-	// In order to reduce conflictions between mods, be sure to alias
-	// functions you wish to overwrite.
-	//---------------------------------------------------------------------
+  ::-webkit-scrollbar-thumb {
+    background: #888;
+    border-radius: 15px;
+  }
 
-	mod: function() {}
-}; // End of module
+  ::-webkit-scrollbar-thumb:hover {
+    background: #555;
+  }
+</style>`;
+
+    document.getElementById('JSEditor').addEventListener('load', function load() {
+      if (document.getElementById('code').value) {
+        this.contentWindow.document.getElementById('hideCode').value = document.getElementById('code').value;
+        // eslint-disable-next-line no-undef
+        this.contentWindow.document.getElementById('hideCode').dispatchEvent(new Event('_load'));
+      }
+      this.style.visibility = 'visible';
+    });
+
+    document.getElementById('code').addEventListener('input', () => {
+      document.getElementById('JSEditor').contentWindow.document.getElementById('hideCode').value =
+        document.getElementById('code').value;
+      // eslint-disable-next-line no-undef
+      document
+        .getElementById('JSEditor')
+        .contentWindow.document.getElementById('hideCode')
+        .dispatchEvent(new Event('_load'));
+    });
+
+    document
+      .getElementById('createAction')
+      .setAttribute(
+        'onclick',
+        'if(document.getElementById("JSEditor").contentWindow.document.getElementById("hideCode").value) document.getElementById("code").value = document.getElementById("JSEditor").contentWindow.document.getElementById("hideCode").value; finish()',
+      );
+
+    glob.onChangeVTypeSelect1 = function onChangeVTypeSelect1() {
+      if (document.getElementById('storage').value === '0') {
+        document.getElementById('VTypeDiv').style.display = 'none';
+      } else {
+        document.getElementById('VTypeDiv').style.display = null;
+      }
+    };
+
+    glob.onChangeVTypeSelect2 = function onChangeVTypeSelect2(element) {
+      if (element.value === '11') {
+        document.getElementById('CVTypeDiv').style.display = null;
+      } else {
+        document.getElementById('CVTypeDiv').style.display = 'none';
+      }
+    };
+
+    glob.onClickDefault = function onClickDefault(element) {
+      if (document.getElementById('code').style.display === 'none') {
+        element.innerHTML = 'Back';
+
+        document.getElementById('code').value = document
+          .getElementById('JSEditor')
+          .contentWindow.document.getElementById('hideCode').value;
+
+        document.getElementById('code').style.display = 'initial';
+        document.getElementById('JSEditor').style.display = 'none';
+      } else {
+        element.innerHTML = 'Default';
+
+        document.getElementById('JSEditor').contentWindow.document.getElementById('hideCode').value =
+          document.getElementById('code').value;
+        // eslint-disable-next-line no-undef
+        document
+          .getElementById('JSEditor')
+          .contentWindow.document.getElementById('hideCode')
+          .dispatchEvent(new Event('_load'));
+
+        document.getElementById('code').style.display = 'none';
+        document.getElementById('JSEditor').style.display = 'initial';
+      }
+    };
+
+    glob.onChangeVTypeSelect1();
+    glob.onClickDefault();
+    glob.onChangeVTypeSelect2(document.getElementById('VTypeSelect'));
+    glob.variableChange(document.getElementById('storage'), 'varNameContainer');
+  },
+
+  action(cache) {
+    const data = cache.actions[cache.index];
+
+    let code;
+    if (data.interpretation === '0') {
+      code = this.evalMessage(data.code, cache);
+    } else {
+      code = data.code;
+    }
+
+    if (data.storage !== '0') {
+      const result = this.eval(code, cache);
+      const varName = this.evalMessage(data.varName, cache);
+      const storage = parseInt(data.storage, 10);
+      this.storeValue(result, storage, varName, cache);
+    } else {
+      this.eval(code, cache);
+    }
+    if (data.behavior === '0') this.callNextAction(cache);
+  },
+
+  mod() {},
+};
